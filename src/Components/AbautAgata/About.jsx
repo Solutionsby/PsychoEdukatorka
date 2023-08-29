adf66374faaa9afbd9b00db3fa50b95862bfd71e
Paweł Goc <pawel.goc@solutionsby.pl>
2023-08-29 02:43:03 +0200

    create folders

--- src/Components/AbautAgata/About.jsx
+++ src/Components/AbautAgata/About.jsx
@@ -1,5 +1,15 @@
 import "./about.scss";
 
 export const About = () => {
-  return <div className="about-wrapper">test</div>;
+  return (
+    <div className="about-wrapper">
+      <div className="about-border">
+        <div className="about-heading">
+          <div className="about-agata-image"></div>
+          <h2>AGATA HEMMERLING-GOC</h2>
+        </div>
+        <div className="about-slider-wrapper"></div>
+      </div>
+    </div>
+  );
 };
